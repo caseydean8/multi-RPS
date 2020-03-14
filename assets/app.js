4274430802a8647721f7639b6eddbc3fc9fda651
Casey Carroll <caseydean8@gmail.com>
2020-03-14 15:43:29 -0400

    Connected to firebase

---
+++ assets/app.js
@@ -0,0 +1,19 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyDolW-LxcVMBEdkPpXoIS_SjkZKtlufnxk",
+  authDomain: "rps-multi-b502c.firebaseapp.com",
+  databaseURL: "https://rps-multi-b502c.firebaseio.com",
+  projectId: "rps-multi-b502c",
+  storageBucket: "rps-multi-b502c.appspot.com",
+  messagingSenderId: "779311652551",
+  appId: "1:779311652551:web:6305d15c2ef76095c668e2",
+  measurementId: "G-0YHG52E7T1"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+const db = firebase.database();
+
+db.ref().on("value", (snapshot) => {
+  console.log(snapshot.val());
+})
